e37b2a86b04d22b0ddccf415c87e965d2fa548e1
Ismail MELLOULI <ismail.mellouli@telecomnancy.eu>
2018-03-30 13:21:07 +0100

    Set app for heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var { USer } = require('./models/user');
 
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -51,8 +52,8 @@ app.get('/todos/:id', (req, res) => {
     //     res.status(404).send();
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started at port ${port}`);
 });
 
 module.exports = { app };
