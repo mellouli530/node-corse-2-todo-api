ade478514b60f7c65fd633cb4285b6f55f13f381
Ismail MELLOULI <ismail.mellouli@telecomnancy.eu>
2018-04-10 14:51:54 +0100

    Setup seperate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config.js');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,7 +11,7 @@ var { USer } = require('./models/user');
 
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
